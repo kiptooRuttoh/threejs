a8a72195d91235dfcc46a70799a8c50f1380b60b
kiptoo <brianrtt@gmail.com>
2025-03-14 16:50:23 +0300

    Animations

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -8,47 +8,129 @@ import { download } from '../assets';
 import { downloadCanvasToImage, reader } from '../config/helpers';
 import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
 import { fadeAnimation, slideAnimation } from '../config/motion';
-import {AIPicker,ColorPicker,CustomButton,FilePicker,Tab} from '../components';
-
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 const Customizer = () => {
   const snap = useSnapshot(state);
+
+  const [file, setFile] = useState('');
+  const [prompt, setPrompt] = useState('');
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  // Show tab content based on the active tab
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />;
+      case "filepicker":
+        return <FilePicker file={file} setFile={setFile} readFile={readFile}/>;
+      case "aipicker":
+        return <AIPicker 
+          prompt={prompt} 
+          setPrompt={setPrompt} 
+          generatingImg={generatingImg} 
+          setGeneratingImg={setGeneratingImg} 
+        />;
+      default:
+        return null;
+    }
+  };
+  const handleDecals = (type, result) => {
+    const decalType = DecalTypes[type];
+
+    state[decalType.stateProperty] = result;
+
+    if(!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab)
+    }
+  }
+  const handleActiveFilterTab = (tabName) => {
+    switch (tabName) {
+      case "logoShirt":
+          state.isLogoTexture = !activeFilterTab[tabName];
+        break;
+      case "stylishShirt":
+          state.isFullTexture = !activeFilterTab[tabName];
+        break;
+      default:
+        state.isLogoTexture = true;
+        state.isFullTexture = false;
+        break;
+    }
+
+    // after setting the state, activeFilterTab is updated
+
+    setActiveFilterTab((prevState) => {
+      return {
+        ...prevState,
+        [tabName]: !prevState[tabName]
+      }
+    })
+  }
+  const readFile = (type) => {
+    reader(file)
+      .then((result) => {
+        handleDecals(type, result);
+        setActiveEditorTab("");
+      })
+  }
+
   return (
     <AnimatePresence>
-      {!snap.intro &&(
+      {!snap.intro && (
         <>
-        <motion.div
-        key="custom"
-        className='absolute top-0 left-0 z-0'
-        {...slideAnimation('left')}
-        >
-          <div className='flex items-center min-h-screen'>
-            <div className='editortabs-container tabs'>
-              {EditorTabs.map((tab)=>(
-                <Tab key={tab.name}tab={tab} handleClick={()=>{}}/>
-              ))}
+          {/* Sidebar for Editor Tabs */}
+          <motion.div
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation('left')}
+          >
+            <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => (
+                  <Tab key={tab.name} tab={tab} handleClick={() => setActiveEditorTab(tab.name)} />
+                ))}
+
+                {generateTabContent()}
+              </div>
             </div>
-          </div>
-        </motion.div>
-        <motion.div className='absolute top-5 right-5 z-0' {...fadeAnimation}>
-          <CustomButton
-          type='filled'
-          title='Go Back'
-          handleClick={()=>state.intro=true}
-          customStyles='w-fit px-4 py-2.5 font-bold text-sm'
-          />
-        </motion.div>
-        <motion.div
-        className='filtertabs-container'
-        {...slideAnimation('up')}
-        >
-            {FilterTabs.map((tab)=>(
-                <Tab key={tab.name}tab={tab}isFilterTab isActiveTab='' handleClick={()=>{}}/>
-              ))}
-        </motion.div>
+          </motion.div>
+
+          {/* Go Back Button */}
+          <motion.div className="absolute top-5 right-5 z-10" {...fadeAnimation}>
+            <CustomButton
+              type="filled"
+              title="Go Back"
+              handleClick={() => (state.intro = true)}
+              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+            />
+          </motion.div>
+
+          {/* Filter Tabs */}
+          <motion.div className="filtertabs-container" {...slideAnimation('up')}>
+            {FilterTabs.map((tab) => (
+              <Tab
+                key={tab.name}
+                tab={tab}
+                isFilterTab
+                isActiveTab={activeFilterTab[tab.name]} // Check if the tab is active
+                handleClick={() =>
+                  setActiveFilterTab((prev) => ({
+                    ...prev,
+                    [tab.name]: !prev[tab.name], // Toggle active state
+                  }))
+                }
+              />
+            ))}
+          </motion.div>
         </>
       )}
     </AnimatePresence>
-  )
-}
+  );
+};
 
-export default Customizer
+export default Customizer;
